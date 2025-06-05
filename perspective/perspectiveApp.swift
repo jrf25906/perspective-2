8bb14c35d04b810de94807bfb2ef56685776db23
jrf25906 <jrf25906@me.com>
2025-06-04 21:07:55 -0400

    Initial Commit

---
+++ perspective/perspectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  perspectiveApp.swift
+//  perspective
+//
+//  Created by James Farmer on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct perspectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
